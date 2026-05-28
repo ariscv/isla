c9db68632077bf64f0636652fd143edf3e23c190
msuad <1445617868@qq.com>
2026-05-29 00:48:43 +0800

    feat(nix): 添加 Z3 和 RISC-V 工具链支持

--- flake.nix
+++ flake.nix
@@ -15,6 +15,17 @@
         rustToolchain = pkgs.rust-bin.stable.latest.default.override {
           extensions = [ "rust-src" ];
         };
+        z3 = pkgs.z3;
+        riscv64-gcc = pkgs.pkgsCross.riscv64.stdenv.cc;
+        riscv64-binutils = pkgs.pkgsCross.riscv64.binutils;
+        riscv64-aliases = pkgs.runCommand "riscv64-linux-gnu-aliases" { } ''
+          mkdir -p "$out/bin"
+          for tool in ${riscv64-gcc}/bin/riscv64-unknown-linux-gnu-* ${riscv64-binutils}/bin/riscv64-unknown-linux-gnu-*; do
+            name="$(basename "$tool")"
+            alias="''${name/riscv64-unknown-linux-gnu-/riscv64-linux-gnu-}"
+            ln -s "$tool" "$out/bin/$alias"
+          done
+        '';
       in
       {
         devShells.default = pkgs.mkShell {
@@ -22,9 +33,15 @@
             rustToolchain
             rust-analyzer
             cargo-watch
+            z3
+            riscv64-gcc
+            riscv64-binutils
+            riscv64-aliases
           ];
 
           RUST_SRC_PATH = "${rustToolchain}/lib/rustlib/src/rust/library";
+          Z3_SYS_Z3_INCLUDE_DIR = "${z3}/include";
+          Z3_SYS_Z3_LIB_DIR = "${z3}/lib";
         };
       }
     );
